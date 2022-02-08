c43a02782f863d2104748e83401d0bfb55664901
LeonardoBai12 <69223422+LeonardoBai12@users.noreply.github.com>
2022-02-08 17:46:08 -0300

    Clean code

--- app/src/main/java/io/lb/firebaseexample/ui/login/LoginActivity.kt
+++ app/src/main/java/io/lb/firebaseexample/ui/login/LoginActivity.kt
@@ -4,17 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.lb.firebaseexample.R
-import io.lb.firebaseexample.databinding.ContentLoginBinding
+import io.lb.firebaseexample.databinding.ActivityLoginBinding
 import io.lb.firebaseexample.ui.main.MainActivity
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: ContentLoginBinding
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_FirebaseExample_NoActionBar)
 
-        binding = ContentLoginBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setupLoginButton()
@@ -22,14 +22,14 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun setupLoginButton() {
-        binding.btLogin.setOnClickListener {
+        binding.included.btLogin.setOnClickListener {
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
         }
     }
 
     private fun setupSignInButton() {
-        binding.btSignIn.setOnClickListener {
+        binding.included.btSignIn.setOnClickListener {
             val i = Intent(this, SignInActivity::class.java)
             startActivity(i)
         }
--- app/src/main/java/io/lb/firebaseexample/ui/login/SignInActivity.kt
+++ app/src/main/java/io/lb/firebaseexample/ui/login/SignInActivity.kt
@@ -3,23 +3,23 @@ package io.lb.firebaseexample.ui.login
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import io.lb.firebaseexample.databinding.ContentSignInBinding
+import io.lb.firebaseexample.databinding.ActivitySignInBinding
 import io.lb.firebaseexample.ui.main.MainActivity
 
 class SignInActivity : AppCompatActivity() {
-    private lateinit var binding: ContentSignInBinding
+    private lateinit var binding: ActivitySignInBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ContentSignInBinding.inflate(layoutInflater)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setupFinishButton()
     }
 
     private fun setupFinishButton() {
-        binding.btFinish.setOnClickListener {
+        binding.included.btFinish.setOnClickListener {
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
         }
