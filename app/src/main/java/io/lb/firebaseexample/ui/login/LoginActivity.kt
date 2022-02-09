5eb20f186c16c3578dc4206b99840e522199485b
LeonardoBai12 <69223422+LeonardoBai12@users.noreply.github.com>
2022-02-09 09:51:49 -0300

    Adicionado login e criação de novo usuário

--- app/src/main/java/io/lb/firebaseexample/ui/login/LoginActivity.kt
+++ app/src/main/java/io/lb/firebaseexample/ui/login/LoginActivity.kt
@@ -2,17 +2,23 @@ package io.lb.firebaseexample.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import io.lb.firebaseexample.R
 import io.lb.firebaseexample.databinding.ActivityLoginBinding
 import io.lb.firebaseexample.ui.main.MainActivity
 import io.lb.firebaseexample.ui.user.UserViewModel
+import timber.log.Timber
+import java.lang.Exception
 import javax.inject.Inject
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private val auth = Firebase.auth
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -31,12 +37,37 @@ class LoginActivity : AppCompatActivity() {
         setupLoginButton()
         setupSignInButton()
     }
-
+    
     private fun setupLoginButton() {
         binding.included.btLogin.setOnClickListener {
-            val i = Intent(this, MainActivity::class.java)
-            startActivity(i)
+            val email = binding.included.tvLoginEmail.editText?.text.toString()
+            val password = binding.included.tvLoginPassword.editText?.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        onSignInSuccess()
+                    } else {
+                        onSignInFailure(task.exception)
+                    }
+                }
         }
+    }
+
+    private fun onSignInSuccess() {
+        Timber.d("signIn:success")
+        val i = Intent(this, MainActivity::class.java)
+        i.putExtra("CURRENT_USER", auth.currentUser)
+        startActivity(i)
+    }
+
+    private fun onSignInFailure(task: Exception?) {
+        Timber.e("signIn:failure $task")
+        Toast.makeText(
+            baseContext,
+            "Authentication failed. $task",
+            Toast.LENGTH_LONG
+        ).show()
     }
 
     private fun setupSignInButton() {
--- app/src/main/java/io/lb/firebaseexample/ui/login/SignInActivity.kt
+++ app/src/main/java/io/lb/firebaseexample/ui/login/SignInActivity.kt
@@ -2,16 +2,22 @@ package io.lb.firebaseexample.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import io.lb.firebaseexample.databinding.ActivitySignInBinding
 import io.lb.firebaseexample.ui.main.MainActivity
 import io.lb.firebaseexample.ui.user.UserViewModel
+import timber.log.Timber
+import java.lang.Exception
 import javax.inject.Inject
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
+    private val auth = Firebase.auth
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -31,8 +37,33 @@ class SignInActivity : AppCompatActivity() {
 
     private fun setupFinishButton() {
         binding.included.btFinish.setOnClickListener {
-            val i = Intent(this, MainActivity::class.java)
-            startActivity(i)
+            val email = binding.included.tvSignInEmail.editText?.text.toString()
+            val password = binding.included.tvSignInPassword.editText?.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        onSignInSuccess()
+                    } else {
+                        onSignInFailure(task.exception)
+                    }
+                }
         }
+    }
+
+    private fun onSignInSuccess() {
+        Timber.d("createUser:success")
+        val i = Intent(this, MainActivity::class.java)
+        i.putExtra("CURRENT_USER", auth.currentUser)
+        startActivity(i)
+    }
+
+    private fun onSignInFailure(task: Exception?) {
+        Timber.e("createUser:failure $task")
+        Toast.makeText(
+            baseContext,
+            "Authentication failed. $task",
+            Toast.LENGTH_LONG
+        ).show()
     }
 }
